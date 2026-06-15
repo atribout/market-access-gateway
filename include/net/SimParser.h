f2a7ea25e8f8b4e1acea6812d97984439229b272
atribout <antoine_tribout@yahoo.fr>
2026-06-15 17:40:34 -0400

    Use Switch instead of if/else if

--- include/net/SimParser.h
+++ include/net/SimParser.h
@@ -13,35 +13,43 @@ public:
         const uint8_t* packet_ptr = payload.data();
         const Sim::PacketHeader* header = reinterpret_cast<const Sim::PacketHeader*>(packet_ptr);
 
-        if (header->type == MsgType::AddOrder && len >= sizeof(Sim::AddOrderMsg)) {
-            const Sim::AddOrderMsg* msg = reinterpret_cast<const Sim::AddOrderMsg*>(packet_ptr);
-            slot->seqNum = std::byteswap(header->seqNum);
-            slot->id = std::byteswap(msg->id);
-            slot->price = std::byteswap(msg->price);
-            slot->quantity = std::byteswap(msg->quantity);
-            slot->instrumentId = std::byteswap(header->instrumentId);
-            slot->type = MsgType::AddOrder;
-            slot->side = (msg->side == 'B') ? Side::Buy : Side::Sell;
-            return true;
-        }
-        else if (header->type == MsgType::CancelOrder && len >= sizeof(Sim::CancelOrderMsg))  {
-            const Sim::CancelOrderMsg* msg = reinterpret_cast<const Sim::CancelOrderMsg*>(packet_ptr);
-            slot->seqNum = std::byteswap(header->seqNum);
-            slot->id = std::byteswap(msg->id);
-            slot->instrumentId = std::byteswap(header->instrumentId);
-            slot->type = MsgType::CancelOrder;
-            return true;
-        }
-        else if (header->type == MsgType::ExecutedOrder && len >= sizeof(Sim::ExecutedOrderMsg))  {
-            const Sim::ExecutedOrderMsg* msg = reinterpret_cast<const Sim::ExecutedOrderMsg*>(packet_ptr);
-            slot->seqNum = std::byteswap(header->seqNum);
-            slot->id = std::byteswap(msg->id);
-            slot->quantity = std::byteswap(msg->quantity);
-            slot->instrumentId = std::byteswap(header->instrumentId);
-            slot->type = MsgType::ExecutedOrder;
-            return true;
-        }
+        switch (header->type) {
+            case MsgType::AddOrder: {
+                if (len < sizeof(Sim::AddOrderMsg)) [[unlikely]] return false;
+                
+                const Sim::AddOrderMsg* msg = reinterpret_cast<const Sim::AddOrderMsg*>(packet_ptr);
+                slot->seqNum = std::byteswap(header->seqNum);
+                slot->id = std::byteswap(msg->id);
+                slot->price = std::byteswap(msg->price);
+                slot->quantity = std::byteswap(msg->quantity);
+                slot->instrumentId = std::byteswap(header->instrumentId);
+                slot->type = MsgType::AddOrder;
+                slot->side = (msg->side == 'B') ? Side::Buy : Side::Sell;
+                return true;
+            }
+            case MsgType::CancelOrder: {
+                if (len < sizeof(Sim::CancelOrderMsg)) [[unlikely]] return false;
 
-        return false; //Unknown type or corrupted packet
+                const Sim::CancelOrderMsg* msg = reinterpret_cast<const Sim::CancelOrderMsg*>(packet_ptr);
+                slot->seqNum = std::byteswap(header->seqNum);
+                slot->id = std::byteswap(msg->id);
+                slot->instrumentId = std::byteswap(header->instrumentId);
+                slot->type = MsgType::CancelOrder;
+                return true;
+            }
+            case MsgType::ExecutedOrder: {
+                if (len < sizeof(Sim::ExecutedOrderMsg)) [[unlikely]] return false;
+
+                const Sim::ExecutedOrderMsg* msg = reinterpret_cast<const Sim::ExecutedOrderMsg*>(packet_ptr);
+                slot->seqNum = std::byteswap(header->seqNum);
+                slot->id = std::byteswap(msg->id);
+                slot->quantity = std::byteswap(msg->quantity);
+                slot->instrumentId = std::byteswap(header->instrumentId);
+                slot->type = MsgType::ExecutedOrder;
+                return true;
+            }
+            default:
+                return false; //Unknown type or corrupted packet
+        }
     }
 };
