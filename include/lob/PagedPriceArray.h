a7114fd0515199e023d93c1983ea0daffede7405
atribout <antoine_tribout@yahoo.fr>
2026-06-22 11:14:57 -0400

    Use Global Page Pool and push allocation out of the hot path

--- include/lob/PagedPriceArray.h
+++ include/lob/PagedPriceArray.h
@@ -1,8 +1,11 @@
+#pragma once
+
 #include <cstdint>
 #include <array>
 #include <vector>
+#include <memory>
 
-template <typename T, size_t MaxPrice, size_t PreallocatedPagesCount = 1024>
+template <typename T, size_t MaxPrice, size_t GlobalPageCount = 32768>
 class PagedPriceArray {
     static_assert(MaxPrice > 0, "MaxPrice must be greater than 0.");
 private:
@@ -15,20 +18,48 @@ private:
         std::array<T, PAGE_SIZE> levels{};
     };
 
-    std::vector<Page> pagePool;
-    size_t nextFreePage{0};
+    static inline std::vector<Page> pagePool{GlobalPageCount};
+    static inline size_t nextFreePage{0uz};
 
-    std::array<Page*, NUM_PAGES> pages;
+    static inline std::vector<std::unique_ptr<Page>> fallbackPool;
+
+    std::array<Page*, NUM_PAGES> pages{};
+
+    __attribute__((noinline)) Page* allocatePage(size_t pageIdx) noexcept {
+        Page* newPage;
+        if (nextFreePage < GlobalPageCount) [[likely]] {
+            newPage = &pagePool[nextFreePage++];
+        } 
+        else {
+            newPage = new Page();
+            fallbackPool.emplace_back(newPage);
+        }
+        pages[pageIdx] = newPage;
+        return newPage;
+    }
+
 public:
-    PagedPriceArray() : pagePool(PreallocatedPagesCount) {}
+    PagedPriceArray() = default;
 
     T& operator[](uint32_t price) noexcept {
         size_t pageIdx = price >> PAGE_SHIFT;
         size_t offset = price & PAGE_MASK;
 
-        if (!pages[pageIdx]) [[unlikely]] {
-            pages[pageIdx] = &pagePool[nextFreePage++];
+        Page* p = pages[pageIdx];
+
+        if (!p) [[unlikely]] {
+            p = allocatePage(pageIdx);
         }
-        return pages[pageIdx]->levels[offset];
+
+        if (!pages[pageIdx]) [[unlikely]] {
+            if (nextFreePage < GlobalPageCount) {
+                pages[pageIdx] = &pagePool[nextFreePage++];
+            }
+            else {
+                pages[pageIdx] = new Page();
+            }
+        }
+            
+        return p->levels[offset];
     }
 };
